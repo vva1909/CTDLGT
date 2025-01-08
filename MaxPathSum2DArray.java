b012ecb47214a8b56ab40707309646cb82c8e5cd
LTVanAnh <lethivananh_t67@hus.edu.vn>
2025-01-08 22:33:50 +0700

    Update code MaxPathSum

--- MaxPathSum2DArray.java
+++ MaxPathSum2DArray.java
@@ -5,17 +5,9 @@ import java.util.Scanner;
 public class MaxPathSum2DArray {
     public static final int N = 1010;
     public static long[][] a = new long[N][N];
-    public static long[][] f = new long[N][N];
+    public static long[][] dp = new long[N][N];
     public static int m, n;
     
-    public static void calculateMaxPathSum() {
-        // Tính toán đường đi lớn nhất
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                f[i][j] = Math.max(f[i - 1][j], f[i][j - 1]) + a[i][j];
-            }
-        }
-    }
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -35,9 +27,20 @@ public class MaxPathSum2DArray {
         calculateMaxPathSum();
 
         // In ra kết quả
-        System.out.println(f[m][n]);
+        System.out.println(dp[m][n]);
 
         scanner.close();
+    }
+    
+    public static void calculateMaxPathSum() {
+        // Tính toán đường đi lớn nhất
+        for (int i = 1; i <= m; i++) {
+            dp[i][1] = a[i][1];
+            for (int j = 1; j <= n; j++) {
+                dp[1][j] = a[1][j];
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]) + a[i][j];
+            }
+        }
     }
 
 }
